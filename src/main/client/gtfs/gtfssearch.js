c30f4725412aab8ebbe4415b2146510c0cbbd25d
Landon Reed <landontreed@gmail.com>
2016-03-14 14:00:48 -0400

    use promise in gtfs search

--- src/main/client/gtfs/gtfssearch.js
+++ src/main/client/gtfs/gtfssearch.js
@@ -62,61 +62,34 @@ export default class GtfsSearch extends React.Component {
       map[obj.id] = obj.shortName !== null ? obj.shortName : obj.name;
       return map;
     }, {})
-
-    const getStops = (input, callback) => {
+    const getStops = (input) => {
       const feedIds = this.props.feeds.map(feed => feed.id)
-      const entities = typeof this.props.entities !== 'undefined' ? this.props.entities : ['routes', 'stops']
-      console.log(entities)
-      // var options;
       const url = input ? `/api/stops?name=${input}&feed=${feedIds.toString()}` : `/api/stops?feed=${feedIds.toString()}`
       return fetch(url)
         .then((response) => {
-          console.log('fetch stops');
-          // console.log(response)
-          // console.log(response.json())
-          // this.state.stops = response.json()
-          return response.json() // .map(stop => ({value: stop.stop_id, label: stop.stop_name}))
+          return response.json()
         })
         .then((json) => {
-          this.setState(Object.assign({}, this.state, { stops: stopOptions }))
           const stopOptions = json.map(stop => ({stop, value: stop.stop_id, label: `${stop.stop_name} (${feedMap[stop.feed_id]})`}))
-          console.log(json)
-          console.log(stopOptions)
           return { options: stopOptions }
         })
-        // .then((options) => callback(input, options))
-      }
-    const getRoutes = (input, options) => {
-      console.log('now fetch routes');
-      console.log('stop options' + options)
-      console.log(options)
+    }
+    const getRoutes = (input) => {
       const feedIds = this.props.feeds.map(feed => feed.id)
       const url = input ? `/api/routes?name=${input}&feed=${feedIds.toString()}` : `/api/routes?feed=${feedIds.toString()}`
       return fetch(url)
         .then((response) => {
-          // console.log(response)
-          // console.log(response.json())
-          // this.state.stops = response.json()
-          return response.json() // .map(stop => ({value: stop.stop_id, label: stop.stop_name}))
+          return response.json()
         })
         .then((json) => {
-          this.setState(Object.assign({}, this.state, { routes: routeOptions }))
           const routeOptions = json.map(route => ({route, value: route.route_id, label: `${route.route_short_name !== null ? route.route_short_name : route.route_long_name} (${feedMap[route.feed_id]})`}))
-          console.log(json)
-          console.log(options)
-          console.log(routeOptions)
-          // option.options = [
-          //   ...option.options,
-          //   routeOptions
-          // ]
           return { options: routeOptions }
         })
-      }
+    }
     const getOptions = (input) => {
-
+      
       const entities = typeof this.props.entities !== 'undefined' ? this.props.entities : ['routes', 'stops']
 
-      // TODO: get this working... instead of what's below
       if (entities.length === 1 && entities[0] === 'stops'){
         console.log('getting stops')
         return getStops(input)
@@ -126,59 +99,20 @@ export default class GtfsSearch extends React.Component {
         return getRoutes(input)
       }
       else{
-        const feedIds = this.props.feeds.map(feed => feed.id)
-      console.log(entities)
-      var options = []
-      const url = input ? `/api/stops?name=${input}&feed=${feedIds.toString()}` : `/api/stops?feed=${feedIds.toString()}`
-      return fetch(url)
-        .then((response) => {
-          console.log('fetch stops');
-          // console.log(response)
-          // console.log(response.json())
-          // this.state.stops = response.json()
-          return response.json() // .map(stop => ({value: stop.stop_id, label: stop.stop_name}))
+        return Promise.all([getStops(input), getRoutes(input)]).then((results) => {
+          const stops = results[0]
+          const routes = results[1]
+          const options = { options: [...stops.options,...routes.options] }
+          console.log('search options', options)
+          return options
         })
-        .then((json) => {
-          this.setState(Object.assign({}, this.state, { stops: stopOptions }))
-          const stopOptions = json.map(stop => ({stop, value: stop.stop_id, label: `${stop.stop_name} (${feedMap[stop.feed_id]})`}))
-          console.log(json)
-          console.log(stopOptions)
-          options = [...stopOptions]
-          // return { options: stopOptions }
-        }).then(() => {
-          console.log('now fetch routes');
-          const url = input ? `/api/routes?name=${input}&feed=${feedIds.toString()}` : `/api/routes?feed=${feedIds.toString()}`
-          return fetch(url)
-            .then((response) => {
-              // console.log(response)
-              // console.log(response.json())
-              // this.state.stops = response.json()
-              return response.json() // .map(stop => ({value: stop.stop_id, label: stop.stop_name}))
-            })
-            .then((json) => {
-              this.setState(Object.assign({}, this.state, { routes: routeOptions }))
-              const routeOptions = json.map(route => ({route, value: route.route_id, label: `${route.route_short_name !== null ? route.route_short_name : route.route_long_name} (${feedMap[route.feed_id]})`}))
-              console.log(json)
-              console.log(routeOptions)
-              // option.options = [
-              //   ...option.options,
-              //   routeOptions
-              // ]
-              return { options: [...options,...routeOptions] }
-          })
-        });
       }
-
-
     }
     const handleChange = (input) => {
       this.onChange(input)
       this.props.onChange(input)
     }
-    const options = [
-      {value: 'one', label: 'One'},
-      {value: 'two', label: 'Two'}
-    ]
+
     const placeholder = 'Begin typing to search for ' + this.props.entities.join(' or ') + '...'
     return (
     <Select.Async
